79411c515432067296f6b1888828efe30e1ba664
nponader <45609244+nponader@users.noreply.github.com>
2024-01-04 16:48:01 +0100

    Improved Weather Forecast HTML and CSS. Injected Weather Forecast via Java Script

--- src/index.js
+++ src/index.js
@@ -51,7 +51,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+            <div class="weather-forecast-day">
+              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-icon"></div>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temp-max">14</span>
+                <span class="weather-forecast-temp-min">12</span>
+              </div>
+            </div>
+            `;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Bayreuth");
+displayForecast();
